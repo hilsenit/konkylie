9345228366d98fb893119360ef1f57b4d384e794
peterhoejlundandersen <hilsenit@gmail.com>
2018-05-22 12:54:52 +0200

    sass is imported and working as it should

--- app/javascript/hello_angular/app/app.component.ts
+++ app/javascript/hello_angular/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import AppHTML from './templates/app.html';
-import './styles/app.component.scss';
+import './styles/app.component.sass';
 
 @Component({
   selector: 'hello-angular',
--- app/javascript/hello_angular/scss.d.ts
+++ app/javascript/hello_angular/scss.d.ts
@@ -1,4 +1,4 @@
-declare module "*.scss" {
+declare module "*.sass" {
   const content: string
   export default content
 }
