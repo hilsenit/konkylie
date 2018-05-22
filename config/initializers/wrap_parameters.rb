b4ef248e88c9648a084e0ee474883abad8e9cee6
peterhoejlundandersen <hilsenit@gmail.com>
2018-05-22 18:03:38 +0200

    changed wrap parameters configuration

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -5,7 +5,8 @@
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
 ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json]
+  # I've CHANGED this
+  wrap_parameters format: []
 end
 
 # To enable root element in JSON for ActiveRecord objects.
